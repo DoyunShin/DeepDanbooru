ee1bf4b22527711b8d7f6b6b7058aac2490301f0
DoyunShin <doyun.shin@gmail.com>
2022-06-02 12:42:45 +0900

    change create table and some things

--- deepdanbooru/commands/create_database.py
+++ deepdanbooru/commands/create_database.py
@@ -21,35 +21,41 @@ def create_database(
     # Open Database
     conn = sqlite3.connect(os.path.join(project_path, "metadata.db"))
     cursor = conn.cursor()
-
-    cursor.execute("""CREATE TABLE posts (
-        id INTEGER PRIMARY KEY,
-        md5 TEXT,
-        file_ext TEXT,
-        tag_string TEXT,
-        tag_count_general INTEGER,
+    try:
+        cursor.execute("""
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY,
+    md5 TEXT,
+    file_ext TEXT,
+    tag_string TEXT,
+    tag_count_general INTEGER
+)
         """)
+    except sqlite3.OperationalError:
+        pass
 
-    count = 0
     insert = []
     for path in json_path_dir_list:
         nowfile = path.split("\\")[-1]
-        f = open(path, "r")
-        for i in path:
+        f = open(path, "r", encoding="utf8")
+        count = 0
+        for i in f:
             data = loads(i)
-
-            insert.append((int(data["id"]), data["md5"], data["file_ext"], data["tag_string"], int(data["tag_count_general"])))
+            try:
+                insert.append((int(data["id"]), data["md5"], data["file_ext"], data["tag_string"], int(data["tag_count_general"])))
+            except KeyError:
+                pass
             if len(insert) == import_size:
                 cursor.executemany("INSERT INTO posts VALUES (?, ?, ?, ?, ?)", insert)
                 conn.commit()
+                print(f"{nowfile} :: {len(insert)} rows imported. {(len(insert) + import_size*count)}")
                 insert = []
-                count = 0
-                print(f"{nowfile} :: {count} row imported.")
+                count += 1
     
     if len(insert) > 0:
         cursor.executemany("INSERT INTO posts VALUES (?, ?, ?, ?, ?)", insert)
         conn.commit()
-        print(f"{nowfile} :: {count} row imported.")
+        print(f"{nowfile} :: {len(insert)} rows imported. {(len(insert) + import_size*count)}")
     
     conn.close()
 
