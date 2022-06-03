5891a1eaa0b74360f3d4529dac38803b28aa5e21
DoyunShin <doyun.shin@gmail.com>
2022-06-03 14:41:17 +0900

    Insert almost thing (Test)

--- deepdanbooru/commands/create_database.py
+++ deepdanbooru/commands/create_database.py
@@ -36,13 +36,41 @@ def create_database(
     cursor = conn.cursor()
     try:
         cursor.execute("""
-CREATE TABLE posts (
+CREATE TABLE posts(
     id INTEGER PRIMARY KEY,
     md5 TEXT,
-    file_ext TEXT,
     tag_string TEXT,
-    tag_count_general INTEGER
+    tag_count INTEGER,
+    tag_string_general TEXT,
+    tag_count_general INTEGER,
+    tag_string_artist TEXT,
+    tag_count_artist INTEGER,
+    tag_string_character TEXT,
+    tag_count_character INTEGER,
+    tag_string_copyright TEXT,
+    tag_count_copyright INTEGER,
+    tag_string_meta TEXT,
+    tag_count_meta INTEGER,
+    rating TEXT,
+    score INTEGER,
+    is_deleted INTEGER,
+    is_banned INTEGER,
+    fav_count INTEGER,
+    file_ext TEXT,
+    uploader_id INTEGER,
+    created_at TEXT,
+    updated_at TEXT,
+    image_width INTEGER,
+    image_height INTEGER,
+    has_children INTEGER,
+    has_active_children INTEGER,
+    has_visible_children INTEGER,
+    file_url TEXT,
+    source TEXT,
 )
+
+
+
         """)
     except sqlite3.OperationalError:
         pass
@@ -59,12 +87,16 @@ CREATE TABLE posts (
             data = loads(data)
 
             try:
-                insert.append((int(data["id"]), data["md5"], data["file_ext"], data["tag_string"], int(data["tag_count_general"])))
+                insert.append((int(data["id"]), data["md5"], data["tag_string"], int(data["tag_count"]), data["tag_string_general"], int(data["tag_count_general"]), data["tag_string_artist"], int(data["tag_count_artist"]), data["tag_string_character"], int(data["tag_count_character"]), data["tag_string_copyright"], int(data["tag_count_copyright"]), data["tag_string_meta"], int(data["tag_count_meta"]), data["rating"], int(data["score"]), int(data["is_deleted"]), int(data["is_banned"]), int(data["fav_count"]), data["file_ext"], int(data["uploader_id"]), data["created_at"], data["updated_at"], int(data["image_width"]), int(data["image_height"]), int(data["has_children"]), int(data["has_active_children"]), int(data["has_visible_children"]), data["file_url"], data["source"]))
+                #insert.append((int(data["id"]), data["md5"], data["file_ext"], data["tag_string"], int(data["tag_count_general"]), data["rating"], int(data["score"]), int(data["is_deleted"])))
+                count += 1
             except KeyError:
                 pass
             if len(insert) == import_size:
                 try:
-                    cursor.executemany("INSERT INTO posts VALUES (?, ?, ?, ?, ?)", insert)
+                    cursor.executemany("""INSERT (id, md5, tag_string, tag_count, tag_string_general, tag_count_general, tag_string_artist, tag_count_artist, tag_string_character, tag_count_character, tag_string_copyright, tag_count_copyright, tag_string_meta, tag_count_meta, rating, score, is_deleted, is_banned, fav_count, file_ext, uploader_id, created_at, updated_at, image_width, image_height, has_children, has_active_children, has_visible_children, file_url, source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", insert)
+                    insert = []
+                    #cursor.executemany("INSERT (id, md5, file_ext, tag_string, tag_count_general, rating, score, is_deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", insert)
                     conn.commit()
                     print(f"{nowfile} :: {len(insert)} rows imported. {(len(insert) + import_size*count)}")
                     count += 1
@@ -80,7 +112,7 @@ CREATE TABLE posts (
 
         if len(insert) > 0:
             try:
-                cursor.executemany("INSERT INTO posts VALUES (?, ?, ?, ?, ?)", insert)
+                cursor.executemany("""INSERT (id, md5, tag_string, tag_count, tag_string_general, tag_count_general, tag_string_artist, tag_count_artist, tag_string_character, tag_count_character, tag_string_copyright, tag_count_copyright, tag_string_meta, tag_count_meta, rating, score, is_deleted, is_banned, fav_count, file_ext, uploader_id, created_at, updated_at, image_width, image_height, has_children, has_active_children, has_visible_children, file_url, source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", insert)
                 conn.commit()
                 print(f"{nowfile} :: {len(insert)} rows imported. {(len(insert) + import_size*count)}")
             except sqlite3.IntegrityError as e:
