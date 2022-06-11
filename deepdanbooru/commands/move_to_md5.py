b464b6fc746337dd0825fcfb46c13b0bdf407f2d
DoyunShin <doyun.shin@gmail.com>
2022-06-11 22:28:00 +0900

    Update move_to_md5.py

--- deepdanbooru/commands/move_to_md5.py
+++ deepdanbooru/commands/move_to_md5.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 from gc import collect
 from hashlib import md5
+from time import sleep
 
 import deepdanbooru as dd
 
-def move_to_md5(source_path, destination_path):
+thread = {}
+
+def move_to_md5(source_path, destination_path, use_threads=False, threads=5):
     """
     Move image to md5 name.
     """
@@ -14,18 +17,47 @@ def move_to_md5(source_path, destination_path):
         raise FileNotFoundError(f"{image_dir} does not exist.")
     
     destination_dir.mkdir(parents=True, exist_ok=True)
-    for image_file in image_dir.glob("**/*"):
-        if image_file.is_file():
-            image_file_md5 = str(md5(image_file.read_bytes()).hexdigest())+image_file.suffix
-            image_file_md5_path = destination_dir / image_file_md5
-            try:
-                image_file.rename(image_file_md5_path)
-            except FileExistsError:
-                print(f"ERROR: {image_file_md5_path} -> {image_file_md5_path} already exists!!")
-                f = open("duplicate_files.txt", "a")
-                f.write(f"{image_file} -> {image_file_md5_path}\n")
-                f.close()
-            print(f"{image_file} -> {image_file_md5_path}")
-            collect()
+    if use_threads:
+        from threading import Thread
+
+        for image_file in image_dir.glob("**/*"):
+            if image_file.is_file():
+                while True:
+                    if len(thread) < threads:
+                        thread.update({str(image_file): Thread(target=move_to_md5_thread, args=(image_file, destination_dir))})
+                        thread[image_file].start()
+                        break
+                    else:
+                        sleep(0.1)
+
+        for thread_id in thread.keys():
+            thread[thread_id].join()
+    else:
+        for image_file in image_dir.glob("**/*"):
+            if image_file.is_file():
+                image_file_md5 = str(md5(image_file.read_bytes()).hexdigest())+image_file.suffix
+                image_file_md5_path = destination_dir / image_file_md5
+                try:
+                    image_file.rename(image_file_md5_path)
+                    print(f"{image_file} -> {image_file_md5_path}")
+                except FileExistsError:
+                    print(f"ERROR: {image_file_md5_path} -> {image_file_md5_path} already exists!!")
+                    f = open("duplicate_files.txt", "a")
+                    f.write(f"{image_file} -> {image_file_md5_path}\n")
+                    f.close()
+                collect()
 
     pass
+
+def move_to_md5_thread(image_file, destination_dir):
+    image_file_md5 = str(md5(image_file.read_bytes()).hexdigest())+image_file.suffix
+    image_file_md5_path = destination_dir / image_file_md5
+    try:
+        image_file.rename(image_file_md5_path)
+        print(f"{image_file} -> {image_file_md5_path}")
+    except FileExistsError:
+        print(f"ERROR: {image_file_md5_path} -> {image_file_md5_path} already exists!!")
+        f = open("duplicate_files.txt", "a")
+        f.write(f"{image_file} -> {image_file_md5_path}\n")
+        f.close()
+    thread.pop(str(image_file))
