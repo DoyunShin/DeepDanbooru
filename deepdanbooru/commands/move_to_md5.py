fbed2ceb05df40da583910131bd103a808293912
DoyunShin <doyun.shin@gmail.com>
2022-06-11 13:23:09 +0900

    Update move_to_md5.py

--- deepdanbooru/commands/move_to_md5.py
+++ deepdanbooru/commands/move_to_md5.py
@@ -16,8 +16,8 @@ def move_to_md5(source_path, destination_path):
     destination_dir.mkdir(parents=True, exist_ok=True)
     for image_file in image_dir.glob("**/*"):
         if image_file.is_file():
-            image_file_md5 = md5(image_file.read_bytes()).hexdigest()
-            image_file_md5_path = destination_dir / image_file_md5 + image_file.suffix
+            image_file_md5 = str(md5(image_file.read_bytes()).hexdigest())+image_file.suffix
+            image_file_md5_path = destination_dir / image_file_md5
             image_file.rename(image_file_md5_path)
             print(f"{image_file} -> {image_file_md5_path}")
             collect()
